b6ee991c0b90512598a504303eeafcf02eb5e717
HefnySco <mohammad.hefny@gmail.com>
2025-06-02 09:13:50 +0300

    fix core de_common

--- src/de_common/de_module.hpp
+++ src/de_common/de_module.hpp
@@ -98,6 +98,7 @@ namespace comm
         
             void sendMSG (const char * msg, const int length)
                 {
+                    if (!cUDPClient.isStarted()) return ;
                     cUDPClient.sendMSG (msg, length);
                 }
 
